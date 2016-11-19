d780eb1ff59c7ff71ebba147d007b655715b5154
Karel Tusek <karel.tusek@hexmotive.com>
2016-11-19 12:34:31 +0100

    Renamed company resource to companies

--- backend/routes.js
+++ backend/routes.js
@@ -6,27 +6,27 @@ module.exports = function(server) {
     resource.health(req, res, next);
   });
 
-  server.get('/company', (req, res, next) => {
+  server.get('/companies', (req, res, next) => {
     var resource = new (require('./lib/resources/company'));
     resource.getAll(req, res, next);
   });
 
-  server.get('/company/:id', (req, res, next) => {
+  server.get('/companies/:id', (req, res, next) => {
     var resource = new (require('./lib/resources/company'));
     resource.getById(req, res, next);
   });
 
-  server.post('/company', (req, res, next) => {
+  server.post('/companies', (req, res, next) => {
     var resource = new (require('./lib/resources/company'));
     resource.create(req, res, next);
   });
 
-  server.put('/company/:id', (req, res, next) => {
+  server.put('/companies/:id', (req, res, next) => {
     var resource = new (require('./lib/resources/company'));
     resource.update(req, res, next);
   });
 
-  server.del('/company/:id', (req, res, next) => {
+  server.del('/companies/:id', (req, res, next) => {
     var resource = new (require('./lib/resources/company'));
     resource.destroy(req, res, next);
   });
